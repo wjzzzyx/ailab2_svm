5a9c58300a074004476a75e77111fc81f78b7c31
zyx <wjszzyx@sohu.com>
2017-06-23 22:17:53 +0800

    use library scikit-learn

--- getFeature.py
+++ getFeature.py
@@ -1,61 +1,38 @@
-import os
-import string
 import numpy as np
-from gensim.models import Word2Vec
-from scipy import sparse
+from gensim.models.keyedvectors import KeyedVectors
 from sklearn.feature_extraction.text import TfidfTransformer
 
-n = 0
 X = []
 y = []
-
-class MySentences():
-    def __init__(self, dirname):
-        self.dirname = dirname
-
-    def __iter__(self):
-        l = os.listdir(self.dirname)
-        l.sort(key=lambda x:int(x.split('_')[0]))
-        for fname in l:
-            with open(os.path.join(self.dirname, fname)) as f:
-                for line in f:
-                    line = line.translate(None, string.punctuation)
-                    yield line.split()
+dim = 300
 
 
-def getFeature(comment, wv):
-    fv = np.zeros(wv.vector_size)
-    for w in comment:
-        fv += wv[w]
-    return fv / len(comment)
+def getFeature(comment):
+    v = np.zeros(dim)
+    count = 0
+    words = comment.split()
+    words = set(words)
+    for word in words:
+        if word in wv:
+            v += wv[word]
+            count += 1
+    if count > 0:
+        v /= count
+    return v
 
+wv = KeyedVectors.load_word2vec_format("word_vectors.txt", binary=False)
 
-dirname = "/home/zyx/PycharmProjects/original"
-try:
-    model = Word2Vec.load("features.model")
-except:
-    sentences = MySentences(dirname)
-    model = Word2Vec(sentences, workers=4)
-    model.save("features.model")
-finally:
-    word_vectors = model.wv
-    del model
-
-for fname in os.listdir(dirname):
-    with open(os.path.join(dirname, fname)) as f:
-        for line in f:
-            line = line.translate(None, string.punctuation)
-            words = line.split()
-            x = getFeature(words, word_vectors)
-            X.append(x)
+with open("input.txt") as f:
+    for line in f:
+        x = getFeature(line)
+        X.append(x)
 
 X = np.array(X)
-with open("data", "r") as f:
+with open("data") as f:
     for line in f:
-        record = [x for x in line.split(' ')]
-        if int(record[0]) >= 7:
+        score = int((line.split())[0])
+        if score >= 7:
             y.append(1)
         else:
             y.append(-1)
-        n += 1
 y = np.array(y)
--- softsvm.py
+++ softsvm.py
@@ -1,9 +1,11 @@
 from math import exp
 import numpy as np
+from sklearn import svm
 from scipy.linalg import *
 from cvxopt import solvers, matrix
 from getFeature import X, y
 
+'''
 def softsvm(traindata, trainlabel, testdata, testlabel, sigma, C):
     n = (traindata.shape)[0]
     trainlabel = trainlabel.reshape(n, 1)
@@ -26,8 +28,8 @@ def softsvm(traindata, trainlabel, testdata, testlabel, sigma, C):
     alpha = solvers.qp(P, q, G, h, A, b)
     print alpha
 
-
 def KMatrix(X, sigma):
+    n = X.shape[0]
     if sigma == 0:
         return X.dot(X.T)
     else:
@@ -36,7 +38,24 @@ def KMatrix(X, sigma):
             for j in xrange(n):
                 t = norm((X.getrow(i) - X.getrow(j)).todense())
                 K[i, j] = exp(- t**2 / sigma**2)
+'''
 
+def softsvm(traindata, trainlabel, testdata, testlabel, sigma, C):
+    if sigma == 0:
+        clf = svm.SVC(kernel='linear', C=C)
+    else:
+        clf = svm.SVC(kernel='rbf')
+    clf.fit(traindata, trainlabel)
+    y_pred = clf.predict(testdata)
+
+    n1 = testdata.shape[0]
+    correct_count = 0
+    for i in xrange(n1):
+        if y_pred[i] == testlabel[i]:
+            correct_count += 1
+
+    print correct_count
+    return y_pred, (correct_count + 0.0) / n1
 
 n = (X.shape)[0]
 softsvm(X[0 : int(0.8 * n)][:], y[0 : int(0.8 * n)], X[int(0.8 * n) : n][:], y[int(0.8 * n) : n], 0, 1)
